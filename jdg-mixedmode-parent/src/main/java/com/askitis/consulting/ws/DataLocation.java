6b5ce588855a9a45d8fa69a348f0ba48aebfb27b
Stelios Kousouris <skousou@gmail.com>
2014-10-23 11:10:44 +0100

    removed binaries

--- jdg-mixedmode-parent/src/main/java/com/askitis/consulting/ws/DataLocation.java
+++
@@ -1,7 +0,0 @@
-package com.askitis.consulting.ws;
-
-public enum DataLocation {
-
-	JDG,
-	DB;
-}
--- jdg-mixedmode-parent/src/main/java/com/askitis/consulting/ws/Mode.java
+++
@@ -1,7 +0,0 @@
-package com.askitis.consulting.ws;
-
-public enum Mode {
-
-	REMOTE,
-	EMBEDDED;
-}
